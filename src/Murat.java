080e6bb9a7116d47d1ae870606c876883fc95b12
BABAYIGIT <babayigit@gmail.com>
2025-05-05 11:21:25 +0300

    babayigit-20250506

--- src/Murat.java
+++ src/Murat.java
@@ -2,5 +2,6 @@ public class Murat {
     public static void main(String[] args) {
         System.out.println("ne yaparsan yap AŞK ile yap");
         System.out.println("Projede kendi branchimde çalışıyorum");
+        System.out.println("Projede 2.gün");
     }
 }
